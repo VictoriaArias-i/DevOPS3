25585a43cb9894399c74a000a71f934de2cd8f0a
VaVi <va.martinoli@duocuc.cl>
2026-06-23 22:33:19 -0400

    fix: use constructor injection in ClienteService

--- src/main/java/Veterinaria/Cliente/Service/ClienteService.java
+++ src/main/java/Veterinaria/Cliente/Service/ClienteService.java
@@ -6,7 +6,6 @@ import Veterinaria.Cliente.Model.Cliente;
 import Veterinaria.Cliente.Repository.ClienteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +15,11 @@ public class ClienteService {
 
     private static final Logger log = LoggerFactory.getLogger(ClienteService.class);
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
     // Retorna todos los clientes registrados
     public List<Cliente> listarTodos() {
